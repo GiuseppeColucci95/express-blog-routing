b4367c0d2999dca3a0d5935c331517cadf1090a3
GiuseppeColucci95 <colucci539@gmail.com>
2025-03-13 15:40:33 +0100

    add posts router

--- app.js
+++ app.js
@@ -7,7 +7,18 @@ const app = express();
 //create server port
 const port = 3000;
 
+//import routers
+const postsRouter = require('./routers/posts');
+
 //start server listening
 app.listen(port, () => {
-  console.log(`Server start running on port http://localhost:${port}`);
+  console.log(`🟢 Server start running on port http://localhost:${port}`);
 });
+
+//home router
+app.get('/', (req, res) => {
+  res.send("Welcome to our Server!");
+});
+
+//posts middleware
+app.use('/posts', postsRouter);
